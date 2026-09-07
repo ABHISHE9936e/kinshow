bd1bd34e5d59065714663745795de2b8f5ff15da
kiinshuk <red2k2green@gmail.com>
2026-09-07 21:15:14 +0530

    Smoother Movies page - remove setTimeout, use requestAnimationFrame

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -7,6 +7,14 @@ import { useInfiniteScroll } from '../hooks/useInfiniteScroll';
 
 const TABS = { popular: 'Popular', top_rated: 'Top Rated', new: 'New Releases (2023+)' };
 
+function getMoviesForTab(tab) {
+  let result = [...MOVIES];
+  if (tab === 'top_rated') result.sort((a, b) => (b.vote_average || 0) - (a.vote_average || 0));
+  else if (tab === 'new') result = result.filter(m => parseInt(m.year) >= 2023);
+  else result.sort((a, b) => (b.vote_average || 0) - (a.vote_average || 0));
+  return result.map(m => ({ ...m, media_type: 'movie' }));
+}
+
 export default function Movies() {
   const [tab, setTab] = useState('popular');
   const [allItems, setAllItems] = useState([]);
@@ -15,14 +23,12 @@ export default function Movies() {
 
   useEffect(() => {
     setLoading(true);
-    setTimeout(() => {
-      let result = [...MOVIES];
-      if (tab === 'top_rated') result.sort((a, b) => (b.vote_average || 0) - (a.vote_average || 0));
-      else if (tab === 'new') result = result.filter(m => parseInt(m.year) >= 2023);
-      else result.sort((a, b) => (b.vote_average || 0) - (a.vote_average || 0));
-      setAllItems(result.map(m => ({ ...m, media_type: 'movie' })));
+    setAllItems([]);
+    const items = getMoviesForTab(tab);
+    requestAnimationFrame(() => {
+      setAllItems(items);
       setLoading(false);
-    }, 300);
+    });
   }, [tab]);
 
   return (
